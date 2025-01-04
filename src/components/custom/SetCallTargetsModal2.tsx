9a01330a9162ac0ef83cecc6f7096b2b4b7ab5d1
jantrej <jantrejbal7@gmail.com>
2025-01-04 20:19:35 +0100

    your changes

--- src/components/custom/SetCallTargetsModal2.tsx
+++ src/components/custom/SetCallTargetsModal2.tsx
@@ -260,10 +260,10 @@ export default function SetCallTargetsModal2() {
             />
           </div>
           <div className="flex-1">
-            <h2 className="text-[20px] md:text-[16px] sm:text-[14px] font-extrabold font-montserrat leading-tight text-[#5b06be] mb-1">
-              Set Performance Goals for Your Team to Unlock Next Avatar
+            <h2 className="text-[23px] md:text-[16px] sm:text-[14px] font-extrabold font-montserrat leading-tight text-[#5b06be] mb-1">
+              Set Team Goals to Unlock Next Avatar
             </h2>
-            <p className="text-[#000000] text-xs sm:text-sm font-montserrat font-bold">
+            <p className="text-[#000000] text-xs sm:text-sm font-montserrat font-semibold">
               Help your team progress by setting minimum performance requirements for their training sessions.
             </p>
           </div>
