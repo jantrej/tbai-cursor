8bdacb941294f2b8b09e4a6856222368f50ce410
jantrej <jantrejbal7@gmail.com>
2025-01-04 19:12:57 +0100

    your changes

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -29,28 +29,26 @@ export default function Header({
   }
 
   return (
-    <div className="w-full h-full bg-[#f2f3f8] p-3">
-      <div className="w-[100%] h-[100%] bg-white flex flex-col rounded-[20px] overflow-hidden p-5">
-        <div className="flex items-start space-x-2">
-          <div className="flex-shrink-0 mt-[2px]">
-            <Image
-              src="https://res.cloudinary.com/drkudvyog/image/upload/v1733749804/Script_icon_qqt4mv.png"
-              alt="Script icon"
-              width={24}
-              height={24}
-              className="object-contain"
-            />
-          </div>
-          <div className="flex-1">
-            <h1 className="text-[18px] md:text-[16px] sm:text-[14px] font-extrabold font-montserrat leading-tight text-[#5b06be] mb-1">
-              {title.endsWith('.') ? title.slice(0, -1) : title}
-            </h1>
-            <p className="text-[14px] md:text-[14px] sm:text-[14px] font-bold font-montserrat text-gray-600 leading-snug">
-              {subheadline}
-            </p>
-          </div>
+    <div className="w-full bg-white rounded-[20px] px-4 py-2">
+      <div className="flex items-start space-x-2">
+        <div className="flex-shrink-0 mt-[2px]">
+          <Image
+            src="https://res.cloudinary.com/drkudvyog/image/upload/v1733749804/Script_icon_qqt4mv.png"
+            alt="Script icon"
+            width={24}
+            height={24}
+            className="object-contain"
+          />
+        </div>
+        <div className="flex-1">
+          <h1 className="text-[18px] md:text-[16px] sm:text-[14px] font-extrabold font-montserrat leading-tight text-[#5b06be] mb-1">
+            {title.endsWith('.') ? title.slice(0, -1) : title}
+          </h1>
+          <p className="text-[14px] md:text-[14px] sm:text-[14px] font-bold font-montserrat text-gray-600 leading-snug">
+            {subheadline}
+          </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
